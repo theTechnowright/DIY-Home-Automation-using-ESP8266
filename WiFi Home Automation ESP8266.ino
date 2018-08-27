dc324bcd25fff65b51feb8d9b1ba2df849096fa0
Shyam Ravi <42342002+theTechnowright@users.noreply.github.com>
2018-08-27 16:03:50 +0430

    Add files via upload

---
+++ WiFi Home Automation ESP8266.ino
@@ -0,0 +1,52 @@
+/*************************************************************
+  By Shyam Ravi- theTechnowright
+  www.youtube.com/thetechnowright 
+  
+  Download latest Blynk library here:
+  https://github.com/blynkkk/blynk-library/releases/latest
+
+  Blynk is a platform with iOS and Android apps to control
+  Arduino, Raspberry Pi and the likes over the Internet.
+  You can easily build graphic interfaces for all your
+  projects by simply dragging and dropping widgets.
+
+  You’ll need:
+   - Blynk App (download from AppStore or Google Play)
+   - ESP8266 board
+   - Decide how to connect to Blynk
+     (USB, Ethernet, Wi-Fi, Bluetooth, ...
+ *************************************************************/
+/* Comment this out to disable prints and save space */
+#define BLYNK_PRINT Serial
+
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+// You should get Auth Token in the Blynk App.
+
+char auth[] = "59b0781d592441ff8e1c100db2675148";//Enter your Auth token here
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "RAVI";           //SSID
+char pass[] = "shyam123";      //Password
+
+void setup()
+{
+  // Debug console
+  Serial.begin(115200); //ESP8266 boards
+
+  //Blynk.begin(auth, ssid, pass);
+  // You can also specify server:
+  Blynk.begin(auth, ssid, pass, "blynk-cloud.com", 8080);
+  //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8080);
+}
+
+void loop()
+{
+  Blynk.run();
+  // You can inject your own code or combine it with other sketches.
+  // Check other examples on how to communicate with Blynk. Remember
+  // to avoid delay() function!
+}
